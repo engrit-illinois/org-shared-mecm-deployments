843b0edec29bc0a95e886b16883c18b8ff0ae09b
Matt Seng <mmseng@users.noreply.github.com>
2021-07-14 15:23:41 -0500

    Update org-shared-deployments-misc.ps1

--- org-shared-deployments-misc.ps1
+++ org-shared-deployments-misc.ps1
@@ -454,3 +454,44 @@ Get-CMCollectionsEvalRuntime -NameQuery "UIUC-ENGR-*" -Collections $colls
 
 # -----------------------------------------------------------------------------
 
+# Re-run a Required Task Sequence
+# https://social.technet.microsoft.com/Forums/en-US/b6eff363-ad8c-4412-b10d-fb70f2ead7f2/how-to-rerun-a-required-deployment?forum=configmanagerosd
+# https://msendpointmgr.com/2012/11/21/re-run-task-sequence-with-powershell/
+# https://smsagent.blog/2014/01/23/re-running-a-task-sequence/
+
+$TsPackageId = "MP0028BE" # You can get the PackageID of the Task Sequence from the MECM Console
+$ComputerName = "computer-name-01"
+
+$scriptBlock = {
+	param(
+		[string]$TsPackageId
+	)
+	$schedule = Get-WmiObject -Namespace "root\ccm\scheduler" -Class ccm_scheduler_history | where {$_.ScheduleID -like "*$TsPackageId*"}
+	if(!$schedule) { Write-Host "Did not find schedule for given Task Sequence!" }
+	else {
+		Write-Host "Found schedule for given Task Sequence."
+		Write-Host "Schedule ID: `"$($schedule.ScheduleID)`"."
+		Write-Host "Deleting schedule..."
+		Get-WmiObject -Namespace "root\ccm\scheduler" -Class ccm_scheduler_history | where {$_.ScheduleID -like "*$TsPackageId*"} | Remove-WmiObject
+		
+		$newSchedule = Get-WmiObject -Namespace "root\ccm\scheduler" -Class ccm_scheduler_history | where {$_.ScheduleID -like "*$TsPackageId*"}
+		if($schedule) { Write-Host "Failed to delete schedule for given Task Sequence!" }
+		else {
+			Write-Host "Successfully deleted schedule for given Task Sequence."
+			Write-Host "Restarting CCMExec..."
+			Get-Service | where {$_.Name -eq "CCMExec"} | Restart-Service
+		}
+	}
+}
+
+Write-Host "Starting PSSession to `"$ComputerName`"..."
+$session = New-PSSession -ComputerName $ComputerName
+Write-Host "Sending commands to session..."
+Invoke-Command -Session $session -ScriptBlock $scriptBlock -ArgumentList $TsPackageId
+Write-Host "Done sending commands to session."
+Write-Host "Ending session..."
+Remove-PSSession $session
+Write-Host "Done."
+
+# -----------------------------------------------------------------------------
+
