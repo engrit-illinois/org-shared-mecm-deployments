4aeeb0961e12edcdf3a457d38b5fac77b68d1114
Matt Seng <mmseng@users.noreply.github.com>
2022-02-28 16:12:06 -0600

    Update org-shared-deployments-misc.ps1

--- org-shared-deployments-misc.ps1
+++ org-shared-deployments-misc.ps1
@@ -13,29 +13,7 @@ Exit
 # -----------------------------------------------------------------------------
 
 # Prepare a connection to SCCM so you can directly use ConfigurationManager Powershell cmdlets without opening the admin console app
-# This is posted as a module in its own repo here: https://github.com/engrit-illinois/Prep-MECM
-function Prep-MECM {
-	$SiteCode = "MP0" # Site code 
-	$ProviderMachineName = "sccmcas.ad.uillinois.edu" # SMS Provider machine name
-
-	# Customizations
-	$initParams = @{}
-	#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
-	#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
-
-	# Import the ConfigurationManager.psd1 module 
-	if((Get-Module ConfigurationManager) -eq $null) {
-		Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
-	}
-
-	# Connect to the site's drive if it is not already present
-	if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
-		New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
-	}
-
-	# Set the current location to be the site code.
-	Set-Location "$($SiteCode):\" @initParams
-}
+# This has been moved to its own repo here: https://github.com/engrit-illinois/Prep-MECM
 
 # -----------------------------------------------------------------------------
 
