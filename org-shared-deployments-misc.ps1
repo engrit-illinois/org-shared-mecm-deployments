d8602e5ec4e335eb45e96b2fc1d83bb1024ec1d8
Matt Seng <mmseng@users.noreply.github.com>
2023-09-01 23:24:40 -0500

    Update org-shared-deployments-misc.ps1

--- org-shared-deployments-misc.ps1
+++ org-shared-deployments-misc.ps1
@@ -898,7 +898,10 @@ function Get-CMDeviceDeployments {
 	}
 }
 
-Get-CMDeviceDeployments "mel-1001-01"
+# Use the function to compare deployments between two devices:
+$left = Get-CMDeviceDeployments "mel-1001-01"
+$right = Get-CMDeviceDeployments "eh-406b1-01"
+Compare-Object -ReferenceObject $left -DifferenceObject $right -IncludeEqual
 
 # -----------------------------------------------------------------------------
 
