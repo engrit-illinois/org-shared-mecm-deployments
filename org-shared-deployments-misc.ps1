7be52e317edc19382a32cd0f34bb6e4cddc05531
Matt Seng <mmseng@users.noreply.github.com>
2024-02-14 15:01:14 -0600

    Update org-shared-deployments-misc.ps1

--- org-shared-deployments-misc.ps1
+++ org-shared-deployments-misc.ps1
@@ -1030,3 +1030,31 @@ $results | Select App,DT,Req | Sort App | Format-Table
 
 # -----------------------------------------------------------------------------
 
+# Procedure for updating MECM AD query-based collections when an OU changes:
+
+# 1. Rename the AD OU, e.g.:
+Rename-ADObject -Identity "OU=ECEB 3014,OU=ECEB,OU=Instructional,OU=Desktops,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu" -NewName "ECEB-3014"
+
+# 2. GpUpdate the computers so their MECM client picks up its new OU (in PS 7+):
+GpUpdate-Computer eceb-3014-*
+
+# 3. Restart MECM agent on endpoints
+$comps = Get-ADComputer -Filter "name -like 'eceb-3014-*'" -SearchBase "OU=ECEB-3014,OU=ECEB,OU=Instructional,OU=Desktops,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu" | Select -ExpandProperty "Name"
+$comps | ForEach-Object { Invoke-Command -ComputerName $_ -ScriptBlock { restart-service ccmexec } }
+
+# 4. Run Discovery Data Collection (a.k.a. Heartbeat Discovery) cycle on clients, using RCT
+# In theory, this can be done via PowerShell as well, through I've not had much luck with it in the past:
+# https://www.anoopcnair.com/trigger-sccm-client-agent-actions-powershell/
+$comps | ForEach-Object { Invoke-WmiMethod -Namespace root\ccm -Class sms_client -Name TriggerSchedule "{00000000-0000-0000-0000-000000000003}" }
+
+# 5. Run the following to make sure all systems are known by MECM to be in their new OU (based on the MECM object's SystemOUName property:
+$test = Get-CMResource -ResourceType System -Fast
+$test | Where { $_.Name -like "eceb-3014-*" } | Sort Name | Select Name,@{N="OU";E={$string=$_.SystemOUName -join "";$string -replace "^.*/",""}}
+
+# 6. If not, wait a few minutes and try again (or try steps 2-4 again), until step 5 shows all systems are recognized as being in the new OU.
+
+# 7. Update the collection membership:
+Invoke-CMCollectionUpdate -Name "UIUC-ENGR-IS ECE ECEB-3014"
+
+# -----------------------------------------------------------------------------
+
